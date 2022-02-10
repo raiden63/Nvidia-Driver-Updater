3dea26d4e215a744445e9a947ee088d60d61ad15
raiden63 <raidon63.cdb@gmail.com>
2022-02-10 14:56:08 -0500

    Bit of cleanup

--- NvidiaClient/NvidiaOption.cs
+++ NvidiaClient/NvidiaOption.cs
@@ -2,8 +2,8 @@ namespace NvidiaDriverUpdater.NvidiaClient
 {
     public class NvidiaOption
     {
-        public string Label { get; set; }
+        public string? Label { get; set; }
 
-        public string Value { get; set; }
+        public string? Value { get; set; }
     }
 }
